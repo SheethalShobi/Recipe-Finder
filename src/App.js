a8816fb2469e5223d4a13beeb1a92d2961755c2b
Sheethal  Shobi <sheethalshobi@gmail.com>
2025-08-14 14:52:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,17 +3,17 @@ import Main from "./Components/Main";
 import { Provider } from "react-redux";
 import { store } from "./Redux/store";
 
-// Backend URL (change to your Flask/Django/Node server address)
-const BACKEND_URL = "http://localhost:5000"; // Flask default port
+
+const BACKEND_URL = "http://localhost:5000"; 
 
 function App() {
   React.useEffect(() => {
-    // Example: Fetch recipes from backend
+    
     fetch(`${BACKEND_URL}/recipes`)
       .then((res) => res.json())
       .then((data) => {
         console.log("Recipes from backend:", data);
-        // Later: Dispatch to Redux store
+       
       })
       .catch((err) => console.error("Error fetching recipes:", err));
   }, []);
